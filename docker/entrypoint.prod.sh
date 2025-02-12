47970a046aa6474399420c4527bac5b178398168
felipebrsk <felipe.ufs97@gmail.com>
2025-02-12 09:09:31 -0300

    fix: removing dev dependencies from prod - removing xdebug - fixing port to 8000

--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-composer install
+composer install --no-dev --optimize-autoloader
 php artisan migrate --force
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
